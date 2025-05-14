2c32e3702b15e59628fc2d4ab0c26ee7b7eb6675
spuffyffets <suchitchaudhari17@gmamil.com>
2025-05-14 17:26:31 +0530

    up com st

---
+++ src/app/add-edit-product/add-edit-product.component.ts
@@ -0,0 +1,147 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../service/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+interface Category {
+  id: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-add-edit-product',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './add-edit-product.component.html',
+  styleUrls: ['./add-edit-product.component.css'],
+})
+export class AddEditProductComponent implements OnInit {
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  productId: string | null = null;
+  name: string = '';
+  sku: string = '';
+  price: string = '';
+  stockQuantity: string = '';
+  categoryId: string = '';
+  description: string = '';
+  isEditing: boolean = false;
+  categories: Category[] = [];
+  message: string = '';
+  loading: boolean = false;
+
+  ngOnInit(): void {
+    this.productId = this.route.snapshot.paramMap.get('productId');
+    this.fetchCategories();
+    if (this.productId) {
+      this.isEditing = true;
+      this.fetchProductById(this.productId);
+    }
+  }
+
+  // Fetch all categories
+  fetchCategories(): void {
+    this.apiService.getAllCategory().subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.categories = res.categories;
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message || error?.message || 'Unable to get categories'
+        );
+      },
+    });
+  }
+
+  // Fetch product details by productId
+  fetchProductById(productId: string): void {
+    this.apiService.getProductById(productId).subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          const product = res.product;
+          this.name = product.name;
+          this.sku = product.sku;
+          this.price = product.price;
+          this.stockQuantity = product.stockQuantity;
+          this.categoryId = product.categoryId;
+          this.description = product.description;
+        } else {
+          this.showMessage(res.message);
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message || error?.message || 'Unable to fetch product'
+        );
+      },
+    });
+  }
+
+  // Handle form submission
+  handleSubmit(event: Event): void {
+    event.preventDefault();
+    this.loading = true;
+    const productData = {
+      name: this.name,
+      sku: this.sku,
+      price: this.price,
+      stockQuantity: this.stockQuantity,
+      categoryId: this.categoryId,
+      description: this.description,
+      productId: this.productId || undefined, // Include productId if editing
+    };
+
+    if (this.isEditing) {
+      // Update product
+      this.apiService.updateProduct(productData).subscribe({
+        next: (res: any) => {
+          if (res.status === 200) {
+            this.showMessage('Product updated successfully');
+            this.router.navigate(['/product']);
+          }
+        },
+        error: (error) => {
+          this.showMessage(
+            error?.error?.message || error?.message || 'Unable to update product'
+          );
+        },
+        complete: () => {
+          this.loading = false;
+        },
+      });
+    } else {
+      // Add new product
+      this.apiService.addProduct(productData).subscribe({
+        next: (res: any) => {
+          if (res.status === 200) {
+            this.showMessage('Product saved successfully');
+            this.router.navigate(['/product']);
+          }
+        },
+        error: (error) => {
+          this.showMessage(
+            error?.error?.message || error?.message || 'Unable to save product'
+          );
+        },
+        complete: () => {
+          this.loading = false;
+        },
+      });
+    }
+  }
+
+  // Show success/error message
+  showMessage(message: string): void {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
+}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,93 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { ApiService } from '../service/api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule, PaginationComponent],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent implements OnInit {
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  products: any[] = [];
+  message: string = '';
+  currentPage: number = 1;
+  totalPages: number = 0;
+  itemsPerPage: number = 10;
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  // FETCH PRODUCTS
+  fetchProducts(): void {
+    this.apiService.getAllProducts().subscribe({
+      next: (res: any) => {
+        const products = res.products || [];
+        console.log(products); // Log to ensure we are fetching data
+
+        this.totalPages = Math.ceil(products.length / this.itemsPerPage);
+
+        this.products = products.slice(
+          (this.currentPage - 1) * this.itemsPerPage,
+          this.currentPage * this.itemsPerPage
+        );
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message || error?.message || 'Unable to fetch products' + error
+        );
+      },
+    });
+  }
+
+  // DELETE A PRODUCT
+  handleProductDelete(productId: string): void {
+    if (window.confirm('Are you sure you want to delete this product?')) {
+      this.apiService.deleteProduct(productId).subscribe({
+        next: (res: any) => {
+          if (res.status === 200) {
+            this.showMessage('Product deleted successfully');
+            this.fetchProducts(); // Reload the products
+          }
+        },
+        error: (error) => {
+          this.showMessage(
+            error?.error?.message ||
+              error?.message ||
+              'Unable to delete product' + error
+          );
+        },
+      });
+    }
+  }
+
+  // HANDLE PAGE CHANGE: Navigate to next, previous, or specific page
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.fetchProducts();
+  }
+
+  // NAVIGATE TO ADD PRODUCT PAGE
+  navigateToAddProductPage(): void {
+    this.router.navigate(['/add-product']);
+  }
+
+  // NAVIGATE TO EDIT PRODUCT PAGE
+  navigateToEditProductPage(productId: string): void {
+    this.router.navigate([`/edit-product/${productId}`]);
+  }
+
+  // SHOW ERROR MESSAGE
+  showMessage(message: string): void {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
+}
