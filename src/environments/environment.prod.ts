c39dd014e3e60675265f6c06b53cc44fc314d01a
spuffyffets <suchitchaudhari17@gmamil.com>
2025-12-09 20:42:11 +0530

    UP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-  // export const environment = {
-  //   production: true,
-  //   baseUrl: 'https://inventory-management-system-txiu.onrender.com/api'  // your live backend URL (e.g. render or any hosting URL)
-  // };
+  export const environment = {
+    production: true,
+    baseUrl: 'https://inventory-management-system-txiu.onrender.com/api'  // your live backend URL (e.g. render or any hosting URL)
+  };
