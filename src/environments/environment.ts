0d658a3a6ec7bdbebd9732d53d62999ed1854f7d
spuffyffets <suchitchaudhari17@gmamil.com>
2025-12-09 20:38:16 +0530

    Update CS 3.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-// export const environment = {
-//   production: false,
-//   baseUrl: 'http://localhost:5050/api'  // your local backend URL like where ur spring projet run
-// };
+export const environment = {
+  production: false,
+  baseUrl: 'http://localhost:5050/api'  // your local backend URL like where ur spring projet run
+};
